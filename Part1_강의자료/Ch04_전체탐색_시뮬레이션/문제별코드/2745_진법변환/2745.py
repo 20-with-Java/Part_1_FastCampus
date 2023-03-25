3386b0a4e7051c8f0816c07e290f32e3a2b73562
eucha09 <euchan5791@naver.com>
2023-03-25 21:24:08 +0900

    upload: ch04 2745 codes(cpp, py)

---
+++ Part1_강의자료/Ch04_전체탐색_시뮬레이션/문제별코드/2745_진법변환/2745.py
@@ -0,0 +1,17 @@
+# N, B = input().split()
+# B = int(B)
+# print(int(N, B))
+
+N, B = input().split()
+B = int(B)
+
+num_dec = 0
+for i in range(len(N)):
+    digit = N[i]
+    num_dec *= B
+    if '0' <= digit <= '9':
+        num_dec += int(digit)
+    else:
+        num_dec += 10 + ord(digit) - ord('A')
+
+print(num_dec)
