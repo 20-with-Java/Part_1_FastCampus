f032ccad6f1625c756ce8b274f63460cc6bb3f40
Sunwoo Song <sunwsong@c2r5s5.42seoul.kr>
2023-03-25 20:47:23 +0900

    upload: ch05 18310 codes(cpp, py)

---
+++ Part1_강의자료/Ch05_정렬/문제별코드/18310_안테나/18310.cpp
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <stdio.h>
+#include <vector>
+
+using namespace std;
+
+int N;
+vector<long long> house;
+
+int main()
+{
+	long long h;
+
+	scanf(" %d", &N);
+	for (int i = 0; i < N; ++i)
+	{
+		scanf(" %lld", &h);
+		house.push_back(h);
+	}
+	sort(house.begin(), house.end());
+	printf("%lld\n", house[(N - 1) / 2]);
+
+	return 0;
+}
