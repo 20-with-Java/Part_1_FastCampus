6e49d202fa102e99de3463ba1c42ec74eaa0a050
Acka HyunJeong Kim <Acka1357@gmail.com>
2023-03-31 00:33:30 +0900

    update: sync code with lecture

--- Part1_강의자료/Ch04_전체탐색_시뮬레이션/문제별코드/2817_ALPS식투표/2817.java
+++ Part1_강의자료/Ch04_전체탐색_시뮬레이션/문제별코드/2817_ALPS식투표/2817.java
@@ -1,26 +1,24 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 class Main
 {
-    static class Score {
-        int staff;
-        double scr;
-        public Score(char staff, double scr) {
-            this.staff = staff;
+    static class Score{
+        Score(int staffIndex, double scr) {
+            this.staffIndex = staffIndex;
             this.scr = scr;
         }
+        int staffIndex;
+        double scr;
     }
 
-    public static void sortScores(Score[] arr) {
+    public static void sortScoresDescendingOrder(Score[] arr) {
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < i; j++) {
-                if (arr[j].scr > arr[i].scr) {
+                if (arr[i].scr > arr[j].scr) {
                     Score cur = arr[i];
                     for (int k = i; k > j; k--)
                         arr[k] = arr[k - 1];
                     arr[j] = cur;
-                    break;
                 }
             }
         }
@@ -30,36 +28,45 @@ class Main
         Scanner sc = new Scanner(System.in);
         int X = sc.nextInt();
         int N = sc.nextInt();
-        char[] staff = new char[N];
-        int[] vote = new int[N];
+
+        // 1. 전체 득표수의 5% 미만의 스태프를 후보에서 제외한다.
+        double validCut = X * 0.05;
+        boolean[] validCandidate = new boolean[26];
+        int[] staffVote = new int[26];
+        int candidateCount = 0;
         for (int i = 0; i < N; i++) {
-            staff[i] = sc.next().charAt(0);
-            vote[i] = sc.nextInt();
+            String name = sc.next();
+            int vote = sc.nextInt();
+            if (vote >= validCut) {
+                int index = name.charAt(0) - 'A';
+                validCandidate[index] = true;
+                staffVote[index] = vote;
+                candidateCount++;
+            }
         }
 
-        double cut = X * 0.05;
-        int cand = 0;
-        for (int i = 0; i < N; i++)
-            if (vote[i] >= cut) cand++;
-
-        Score[] scores = new Score[cand * 14];
-        int score_idx = 0;
-        for (int i = 0; i < N; i++)
-            if (vote[i] >= cut) {
+        // 2. 남은 스태프마다 받은 득표수를 1~14로 나눈 점수 집합을 구한다.
+        Score[] scores = new Score[candidateCount * 14];
+        int scoreIndex = 0;
+        for (int i = 0 ; i < 26; i++) {
+            if (validCandidate[i]) {
                 for (int j = 1; j <= 14; j++)
-                    scores[score_idx++] = new Score(staff[i], (double) vote[i] / j);
+                    scores[scoreIndex++] = new Score(i, (double)staffVote[i] / j);
             }
+        }
 
-        sortScores(scores);
+        // 3. 전체 점수 집합에서 점수가 큰 1~14번째 스태프에게 칩을 1개씩 지급한다.
+        sortScoresDescendingOrder(scores);
+
         int[] ans = new int[26];
-        Arrays.fill(ans, -1);
-        for (int i = 0; i < N; i++)
-            if (vote[i] >= cut) ans[staff[i] - 'A'] = 0;
         for (int i = 0; i < 14; i++)
-            ans[scores[scores.length - i - 1].staff - 'A']++;
+            ans[scores[i].staffIndex]++;
 
-        for (int i = 0; i < 26; i++)
-            if (ans[i] >= 0)
-                System.out.println((char)('A' + i) + " " + ans[i]);
+        // 4. 스태프 이름에 대해 사전순으로 후보 스태프와 받은 칩의 수를 출력한다.
+        for (int i = 0; i < 26; i++) {
+            if (validCandidate[i])
+                System.out.println((char)(i + 'A') + " " + ans[i]);
+        }
+
     }
 }
