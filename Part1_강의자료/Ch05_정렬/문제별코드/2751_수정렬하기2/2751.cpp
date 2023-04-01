2bad12652d15d37c0f8c9a29389c1cf1541d6292
eucha09 <euchan5791@naver.com>
2023-04-01 19:12:53 +0900

    upload: ch05 2751 codes(cpp, py)

---
+++ Part1_강의자료/Ch05_정렬/문제별코드/2751_수정렬하기2/2751.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int N;
+    vector<int> v;
+
+    scanf("%d", &N);
+    v.resize(N);
+    for(int i = 0; i < N; i++)
+        scanf("%d", &v[i]);
+
+    sort(v.begin(), v.end());
+
+    for(int i = 0; i < N; i++)
+        printf("%d\n", v[i]);
+    return 0;
+}
