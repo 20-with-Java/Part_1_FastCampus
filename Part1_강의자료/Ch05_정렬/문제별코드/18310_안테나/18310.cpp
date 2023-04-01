7b1f2958f33938dfb747f5562b7b43732e4a850a
Sunwoo Song <sunwsong@c2r6s5.42seoul.kr>
2023-04-01 19:39:31 +0900

    modify: ch05 18310 cpp code

--- Part1_강의자료/Ch05_정렬/문제별코드/18310_안테나/18310.cpp
+++ Part1_강의자료/Ch05_정렬/문제별코드/18310_안테나/18310.cpp
@@ -9,16 +9,16 @@ vector<long long> house;
 
 int main()
 {
-	long long h;
+    long long h;
 
-	scanf(" %d", &N);
-	for (int i = 0; i < N; ++i)
-	{
-		scanf(" %lld", &h);
-		house.push_back(h);
-	}
-	sort(house.begin(), house.end());
-	printf("%lld\n", house[(N - 1) / 2]);
+    scanf("%d", &N);
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%lld", &h);
+        house.push_back(h);
+    }
+    sort(house.begin(), house.end());
+    printf("%lld\n", house[(N - 1) / 2]);
 
-	return 0;
+    return 0;
 }
