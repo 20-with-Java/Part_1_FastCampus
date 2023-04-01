5e59041f8cfe9ed2b5fada3eab08b2f493ea56c4
Sunwoo Song <sunwsong@c2r6s5.42seoul.kr>
2023-04-01 20:30:49 +0900

    modify: name arr to vec ch05 2751 cpp code

--- Part1_강의자료/Ch05_정렬/문제별코드/2751_수정렬하기2/2751.cpp
+++ Part1_강의자료/Ch05_정렬/문제별코드/2751_수정렬하기2/2751.cpp
@@ -12,12 +12,12 @@ int main()
     vec.resize(N);
 
     for (int i = 0; i < N; i++)
-        scanf("%d", &arr[i]);
+        scanf("%d", &vec[i]);
 
-    sort(arr.begin(), arr.end());
+    sort(vec.begin(), vec.end());
 
     for (int i = 0; i < N; i++)
-        printf("%d\n", arr[i]);
+        printf("%d\n", vec[i]);
 
     return 0;
 }
