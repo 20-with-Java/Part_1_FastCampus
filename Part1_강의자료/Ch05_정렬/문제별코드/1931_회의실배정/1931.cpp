f86c3eedd46dda358151110f03d7eb1f20d79cda
Sunwoo Song <sunwsong@c2r6s5.42seoul.kr>
2023-04-01 19:49:59 +0900

    modify: ch05 1931 cpp code

--- Part1_강의자료/Ch05_정렬/문제별코드/1931_회의실배정/1931.cpp
+++ Part1_강의자료/Ch05_정렬/문제별코드/1931_회의실배정/1931.cpp
@@ -6,8 +6,8 @@ using namespace std;
 typedef long long ll;
 
 typedef struct {
-	ll start;
-	ll end;
+    ll start;
+    ll end;
 } meeting;
 
 int N;
@@ -20,22 +20,22 @@ bool cmp(meeting a, meeting b)
 
 int main()
 {
-	scanf("%d", &N);
+    scanf("%d", &N);
 
-	for (int i = 0; i < N; i++)
-		scanf("%lld %lld", &meetings[i].start, &meetings[i].end);
-	sort(meetings, meetings + N, cmp);
+    for (int i = 0; i < N; i++)
+        scanf("%lld %lld", &meetings[i].start, &meetings[i].end);
+    sort(meetings, meetings + N, cmp);
 
     ll end_time = 0, res = 0;
-	for (int i = 0; i < N; i++)
+    for (int i = 0; i < N; i++)
     {
-		if (end_time <= meetings[i].start) 
+        if (end_time <= meetings[i].start) 
         {
-			res++;
-			end_time = meetings[i].end;
-		}
-	}
+            res++;
+            end_time = meetings[i].end;
+        }
+    }
 
-	printf("%lld\n", res);
-	return 0;
+    printf("%lld\n", res);
+    return 0;
 }
