4b4d89673e95be8c1b96d215314dd3cb9c366825
eucha09 <euchan5791@naver.com>
2023-04-04 21:08:56 +0900

    modify: ch05 10814.cpp(operator)

--- Part1_강의자료/Ch05_정렬/문제별코드/10814_나이순정렬/10814.cpp
+++ Part1_강의자료/Ch05_정렬/문제별코드/10814_나이순정렬/10814.cpp
@@ -4,18 +4,16 @@
 #include <algorithm>
 using namespace std;
 
-typedef struct {
+struct member {
     int age;
     string name;
     int idx;
-} member;
-
-bool compare(member &a, member &b)
-{
-    if (a.age != b.age)
-        return a.age < b.age;
-    return a.idx < b.idx;
-}
+    bool operator<(const member &o) const{
+        if (age != o.age)
+            return age < o.age;
+        return idx < o.idx;
+    }
+};
 
 int main()
 {
@@ -25,16 +23,13 @@ int main()
     char name[101];
 
     scanf("%d", &N);
-    members.resize(N);
     for (int i = 0; i < N; i++)
     {
         scanf("%d %s", &age, name);
-        members[i].age = age;
-        members[i].name = name;
-        members[i].idx = i;
+        members.push_back({age, name, i});
     }
 
-    sort(members.begin(), members.end(), compare);
+    sort(members.begin(), members.end());
 
     for (int i = 0; i < N; i++)
         printf("%d %s\n", members[i].age, members[i].name.c_str());
