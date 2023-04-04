56f214001fe11e756c56f8d08ab3c689d2015adf
eucha09 <euchan5791@naver.com>
2023-04-04 16:06:46 +0900

    modify: ch05 10814.cpp

--- Part1_강의자료/Ch05_정렬/문제별코드/10814_나이순정렬/10814.cpp
+++ Part1_강의자료/Ch05_정렬/문제별코드/10814_나이순정렬/10814.cpp
@@ -20,24 +20,24 @@ bool compare(member &a, member &b)
 int main()
 {
     int N;
-    vector<member> v;
+    vector<member> members;
     int age;
     char name[101];
 
     scanf("%d", &N);
-    v.resize(N);
-    for(int i = 0; i < N; i++)
+    members.resize(N);
+    for (int i = 0; i < N; i++)
     {
         scanf("%d %s", &age, name);
-        v[i].age = age;
-        v[i].name = name;
-        v[i].idx = i;
+        members[i].age = age;
+        members[i].name = name;
+        members[i].idx = i;
     }
 
-    sort(v.begin(), v.end(), compare);
+    sort(members.begin(), members.end(), compare);
 
-    for(int i = 0; i < N; i++)
-        printf("%d %s\n", v[i].age, v[i].name.c_str());
+    for (int i = 0; i < N; i++)
+        printf("%d %s\n", members[i].age, members[i].name.c_str());
 
     return 0;
 }
