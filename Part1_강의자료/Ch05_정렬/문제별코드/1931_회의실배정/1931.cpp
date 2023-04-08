b5c8f796d1356cf56b0e3787a6b7647fab35d8bb
Sunwoo Song <sunwsong@c2r5s5.42seoul.kr>
2023-04-08 20:19:09 +0900

    modify: ch05 1931.cpp struct name, lambda

--- Part1_강의자료/Ch05_정렬/문제별코드/1931_회의실배정/1931.cpp
+++ Part1_강의자료/Ch05_정렬/문제별코드/1931_회의실배정/1931.cpp
@@ -4,27 +4,24 @@
 
 using namespace std;
 
-typedef struct {
+struct Meeting {
     int start;
     int end;
-} meeting;
-
-bool cmp(meeting a, meeting b) 
-{ 
-    return a.end == b.end ? a.start < b.start : a.end < b.end;
-}
+};
 
 int main()
 {
     int N;
-    vector<meeting> meetings;
     
     scanf("%d", &N);
-    meetings.resize(N);
+    vector<Meeting> meetings(N);
 
     for (int i = 0; i < N; i++)
         scanf("%d %d", &meetings[i].start, &meetings[i].end);
-    sort(meetings.begin(), meetings.end(), cmp);
+        
+    sort(meetings.begin(), meetings.end(), [](const Meeting &a, const Meeting &b) {
+        return a.end == b.end ? a.start < b.start : a.end < b.end;
+    });
 
     int end_time = 0, res = 0;
     for (int i = 0; i < N; i++)
