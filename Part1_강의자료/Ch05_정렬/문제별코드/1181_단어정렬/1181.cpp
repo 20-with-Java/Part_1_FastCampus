cc4eb39ed594d35d153d92788fd622f36c83e5f7
Sunwoo Song <sunwsong@c2r5s5.42seoul.kr>
2023-04-08 19:38:51 +0900

    modify: ch05 1171 codes(cpp, py)

--- Part1_강의자료/Ch05_정렬/문제별코드/1181_단어정렬/1181.cpp
+++ Part1_강의자료/Ch05_정렬/문제별코드/1181_단어정렬/1181.cpp
@@ -14,19 +14,21 @@ int main()
 {
     char str[55];
     int N;
-    vector<string> vec;
 
     scanf("%d", &N);
+    vector<string> vec(N);
     for (int i = 0; i < N; i++)
     {
         scanf("%s", str);
-        vec.push_back(str);
+        vec[i] = str;
     }
 
     sort(vec.begin(), vec.end(), cmp);
-    vec.erase(unique(vec.begin(), vec.end()), vec.end());
 
-    for (auto & it : vec)
-        printf("%s\n", it.c_str());
+    printf("%s\n", vec[0].c_str());
+    for (int i = 1; i < vec.size(); i++)
+        if (vec[i - 1] != vec[i])
+            printf("%s\n", vec[i].c_str());
+    
     return 0;
 }
--- Part1_강의자료/Ch05_정렬/문제별코드/1181_단어정렬/1181.py
+++ Part1_강의자료/Ch05_정렬/문제별코드/1181_단어정렬/1181.py
@@ -2,10 +2,8 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-lst = [input() for _ in range(N)]
-lst = list(set(lst))
+lst = list(set([input() for _ in range(N)]))
 
 lst.sort(key=lambda x: (len(x), x))
 
-for str in lst:
-    print(str, end='')
+print(''.join(lst))
