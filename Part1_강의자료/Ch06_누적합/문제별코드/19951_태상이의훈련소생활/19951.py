7951e021a566f37509295540dfe54af79a21b7e6
Sunwoo Song <sunwsong@c2r6s4.42seoul.kr>
2023-04-09 10:47:55 +0900

    modify: ch06 19951.py list

--- Part1_강의자료/Ch06_누적합/문제별코드/19951_태상이의훈련소생활/19951.py
+++ Part1_강의자료/Ch06_누적합/문제별코드/19951_태상이의훈련소생활/19951.py
@@ -2,7 +2,7 @@ import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-li = [0] + list(map(int, input().split()))
+li = [[0] * (N + 1)] + list(map(int, input().split()))
 acc = [0] * (N + 2)
 
 for _ in range(M):
