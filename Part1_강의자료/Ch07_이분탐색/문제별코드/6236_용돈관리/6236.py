11a2b31f0d12356bbd216985f96ef29554ce2a75
Sunwoo Song <sunwsong@c8r3s13.42seoul.kr>
2023-04-14 15:28:37 +0900

    modify: ch07 6236.py complete

--- Part1_강의자료/Ch07_이분탐색/문제별코드/6236_용돈관리/6236.py
+++ Part1_강의자료/Ch07_이분탐색/문제별코드/6236_용돈관리/6236.py
@@ -1,25 +1,23 @@
 import sys
 input = sys.stdin.readline
 
-N, K = map(int, input().split())
-li = [int(input()) for _ in range(N)]
+N, M = map(int, input().split())
+use_amounts = [int(input()) for _ in range(N)]
 
-def lower_bound(mid):
-	cnt = 1
-	cur = mid
-	for i in li:
-		cur -= i
-		if cur < 0:
-			cur = mid - i
-			cnt += 1
-	return cnt <= K
+def is_possible(mid):
+	draw_cnt, current_amount = 1, mid
+	for use_amount in use_amounts:
+		current_amount -= use_amount
+		if current_amount < 0:
+			current_amount = mid - use_amount
+			draw_cnt += 1
+	return draw_cnt <= M
 
-l, r = max(li), sum(li)
+l, r = max(use_amounts), sum(use_amounts)
 while l < r:
-	mid = (l + r) >> 1
-	if lower_bound(mid):
+	mid = (l + r) // 2
+	if is_possible(mid):
 		r = mid
 	else:
 		l = mid + 1
-
 print(l)
